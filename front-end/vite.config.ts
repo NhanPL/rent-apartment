738a3dd8f928083c147fa32705b54e0034125430
nhanpl <dvnhan1999@gmail.com>
2026-08-12 22:30:07 +0900

    fix: stabilize production vendor chunks

--- front-end/vite.config.ts
+++ front-end/vite.config.ts
@@ -10,8 +10,15 @@ export default defineConfig({
         codeSplitting: {
           groups: [
             { name: 'react-vendor', test: /node_modules[\\/](?:react|react-dom|scheduler)[\\/]/ },
-            { name: 'antd-vendor', test: /node_modules[\\/](?:antd|@ant-design)[\\/]/, maxSize: 420_000 },
-            { name: 'rc-vendor', test: /node_modules[\\/]rc-[^\\/]+[\\/]/, maxSize: 420_000 },
+            { name: 'antd-core', test: /node_modules[\\/]antd[\\/]es[\\/]_(?:util|shared)[\\/]/ },
+            { name: 'antd-icons', test: /node_modules[\\/]@ant-design[\\/](?:icons|icons-svg)[\\/]/ },
+            { name: 'antd-data-entry', test: /node_modules[\\/]antd[\\/]es[\\/](?:auto-complete|cascader|checkbox|color-picker|date-picker|form|input|input-number|mentions|radio|rate|select|slider|switch|time-picker|transfer|tree-select|upload)[\\/]/ },
+            { name: 'antd-data-display', test: /node_modules[\\/]antd[\\/]es[\\/](?:avatar|badge|calendar|card|carousel|collapse|descriptions|empty|image|list|popover|qrcode|statistic|table|tag|timeline|tooltip|tree)[\\/]/ },
+            { name: 'antd-feedback', test: /node_modules[\\/]antd[\\/]es[\\/](?:alert|drawer|message|modal|notification|popconfirm|progress|result|skeleton|spin)[\\/]/ },
+            { name: 'antd-navigation', test: /node_modules[\\/]antd[\\/]es[\\/](?:anchor|breadcrumb|dropdown|menu|pagination|steps|tabs)[\\/]/ },
+            { name: 'antd-layout', test: /node_modules[\\/]antd[\\/]es[\\/](?:app|button|divider|flex|float-button|grid|layout|space|splitter|typography)[\\/]/ },
+            { name: 'antd-vendor', test: /node_modules[\\/]antd[\\/]/ },
+            { name: 'rc-vendor', test: /node_modules[\\/](?:@rc-component|rc-[^\\/]+)[\\/]/ },
             { name: 'query-vendor', test: /node_modules[\\/]@tanstack[\\/]/ },
             { name: 'i18n-vendor', test: /node_modules[\\/](?:i18next|react-i18next)[\\/]/ },
           ],
